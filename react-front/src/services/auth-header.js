266495880fe81acdbfab9a25c653b8082a7d57bf
paquisha <paquishaxd@gmail.com>
2024-07-25 01:09:42 -0500

    se agrega front de prueba para deber de sistemas distribuidos

---
+++ react-front/src/services/auth-header.js
@@ -0,0 +1,11 @@
+export default function authHeader() {
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  if (user && user.accessToken) {
+    // for Node.js Express back-end
+    return { Authorization: 'Bearer ' + user.accessToken };
+    //return { 'x-access-token': user.accessToken };
+  } else {
+    return {};
+  }
+}
